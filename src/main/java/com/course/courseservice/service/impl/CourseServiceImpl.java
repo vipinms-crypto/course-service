b1e25a65c2bd66f9e69c34789cf2de072c4c1905
Vipin.Sukumaran <Vipin.Sukumaran@host.docker.internal>
2025-01-02 11:07:30 +0530

    logs and log fix in pom

--- src/main/java/com/course/courseservice/service/impl/CourseServiceImpl.java
+++ src/main/java/com/course/courseservice/service/impl/CourseServiceImpl.java
@@ -10,11 +10,15 @@ import org.springframework.stereotype.Service;
 import com.course.courseservice.dto.requestDto.CourseRequestDto;
 import com.course.courseservice.dto.responseDto.CourseResponseDto;
 import com.course.courseservice.entity.Course;
+import com.course.courseservice.exception.ResourceNotFoundException;
 import com.course.courseservice.mapper.CourseMapper;
 import com.course.courseservice.repository.CourseRepository;
 import com.course.courseservice.service.CourseService;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Service
+@Slf4j
 public class CourseServiceImpl implements CourseService {
 	
 	@Autowired
@@ -25,13 +29,16 @@ public class CourseServiceImpl implements CourseService {
 
     @Override
     public CourseResponseDto createCourse(CourseRequestDto courseRequestDto) throws Exception {
+    	log.info("Entered into service method createCourse");
         Course course = courseMapper.toEntity(courseRequestDto);
         Course savedCourse = courseRepository.save(course);
+        log.info("Exit from service method createCourse");
         return mapToResponseDto(savedCourse);
     }
 
     @Override
     public List<CourseResponseDto> getAllCourses() throws Exception{
+    	log.info("Entered into service method getAllCourses");
         return courseRepository.findAll().stream()
                 .map(this::mapToResponseDto)
                 .collect(Collectors.toList());
@@ -39,34 +46,42 @@ public class CourseServiceImpl implements CourseService {
 
     @Override
     public Optional<CourseResponseDto> getCourseById(Integer id) throws Exception{
+    	log.info("Entered into service method getCourseById");
         return courseRepository.findById(id)
                 .map(this::mapToResponseDto);
     }
 
     @Override
     public CourseResponseDto updateCourse(Integer id, CourseRequestDto courseRequestDto) throws Exception{
+    	log.info("Entered into service method updateCourse");
         Optional<Course> courseOptional = courseRepository.findById(id);
         if (courseOptional.isPresent()) {
             Course course = courseOptional.get();
             course = courseMapper.toEntity(courseRequestDto);
             Course updatedCourse = courseRepository.save(course);
+            log.info("Exit from service method updateCourse with response ,"+ updatedCourse);
             return  courseMapper.toDto(updatedCourse);
         } else {
-            return null; // Or throw an exception
+        	log.info("No Course data found for given id,"+id);
+        	throw new ResourceNotFoundException("No data found");
         }
     }
 
     @Override
     public boolean deleteCourse(Integer id) throws Exception{
+    	log.info("Entered into service method deleteCourse");
         if (courseRepository.existsById(id)) {
             courseRepository.deleteById(id);
+            log.info("Entered into service method deleteCourse");
             return true;
         } else {
-            return false; // Or throw an exception
+        	log.info("No Course data found for given id,"+id);
+        	throw new ResourceNotFoundException("No data found");
         }
     }
 
     private CourseResponseDto mapToResponseDto(Course course) {
+    	log.info("Entered into service method mapToResponseDto");
     	return courseMapper.toDto(course);
     }
 }
